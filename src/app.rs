329b3c434bb77006136afcb9162d53099afdf85b
QingWei <l.q.wei91@gmail.com>
2023-06-11 10:25:44 +0100

    Init commit
    * Manage to draw box with pan and zoom
    * Manage to compile simplest relationship to data struct
    
    Next:
    1. Stich things up so that I can have basic e2e
    2. Then implement layers
    3. Then tinker on deploying wasm page

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,131 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use std::ops::{Add, Deref};
+use eframe::epaint::RectShape;
+use egui::{Color32, Event, Id, InputState, Pos2, pos2, Rect, Rounding, ScrollArea, Sense, Shape, Stroke, Style, Ui, Vec2, vec2};
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+/*
+Data structured in layers:
+Every layer contains one or more boxes, every box can transform into another layer, ie. every layer has 1 parent, except root layer
+
+We need to know
+1. WHich layer we are at
+1. For each layer, we need to know what is in there, and pan offset and scale
+
+One problem not solved yet, if we use scroll to zoom into next layer, we need to decide `Which` layer to zoom into
+It is easy if the zoom focal is within a box, but what to do if focal is not within any box? I can think of 2 choices
+a. Dont use scroll to zoom into layer, use double click on a box
+b. Pick the closest box by mid point
+*/
+
+struct RootLayer {
+    boxes: Vec<Abox>,
+    ui_config: UIConfig,
+}
+
+impl RootLayer {
+    fn draw(&mut self, ui: &mut Ui) {
+        let (res, painter) = ui.allocate_painter(ui.available_size_before_wrap(), Sense::drag());
+
+        // this is stupid, events will be return after this scope
+        let zoom_delta = ui.input(|i| {
+            i.events.clone().into_iter()
+                .find_map(|e| match e {
+                    Event::MouseWheel { delta, .. } => Some(delta.y),
+                    _ => None
+                })
+        });
+
+        if zoom_delta.is_some() {
+            ui.ctx().pointer_latest_pos().map(|zoom_p| {
+                let before = self.ui_config.screen_2_world(zoom_p);
+                self.ui_config.pan_zoom(res.drag_delta(), zoom_delta.unwrap_or(0.0));
+                let after = self.ui_config.screen_2_world(zoom_p);
+                let displacement = after - before;
+                self.ui_config.pan_offset -= displacement;
+            });
+        } else {
+            self.ui_config.pan_zoom(res.drag_delta(), 0.0);
+        }
+
+        for b in self.boxes.iter_mut() {
+            let size = b.size * self.ui_config.zoom_factor;
+            let box_a = Rect::from_min_size(self.ui_config.world_2_screen(b.pos), size);
+            painter.rect(box_a, Rounding::none(), Color32::RED, Stroke::default());
+        };
+    }
+}
+
+struct ChildLayer {
+    boxes: Vec<Abox>,
+    ui_config: UIConfig,
+    parent: Box<Layer>,
+}
+
+enum Layer {
+    Root(RootLayer),
+    Child(ChildLayer),
+}
+
+impl Layer {
+    fn draw(&mut self, ui: &mut Ui) {
+        match self {
+            Layer::Root(rootLayer) => {}
+            Layer::Child(_) => {}
+        }
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct Abox {
+    pos: Pos2,
+    size: Vec2,
+    text: String,
+}
+
+impl Abox {
+    fn into(&self, uiStyle: &UIConfig) -> Shape {
+        todo!()
+    }
+}
+
+struct UIConfig {
+    zoom_factor: f32,
+    pan_offset: Vec2,
+}
+
+impl UIConfig {
+    fn pan_zoom(&mut self, drag_delta: Vec2, zoom_factor_delta: f32) {
+        self.pan_offset = self.pan_offset - drag_delta;
+
+        let o = self.zoom_factor + zoom_factor_delta;
+        if o > 0.01 && o < 2.0 {
+            self.zoom_factor = o
+        }
+    }
+
+    fn world_2_screen(&mut self, i: Pos2) -> Pos2 {
+        return ((i - self.pan_offset).to_vec2() * self.zoom_factor).to_pos2();
+    }
+
+    fn screen_2_world(&mut self, i: Pos2) -> Pos2 {
+        return ((i.to_vec2() / self.zoom_factor) + self.pan_offset).to_pos2();
+    }
+}
+
+pub struct TemplateApp {
+    root: RootLayer,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
+        let all_boxes = (0..4).map(|i| {
+            Abox { pos: pos2(100.0 * i as f32, 100.0 * i as f32), size: vec2(20.0, 20.0), text: String::from("ooo") }
+        }).collect();
+
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            root: RootLayer {
+                boxes: all_boxes,
+                ui_config: UIConfig { zoom_factor: 1.0, pan_offset: vec2(0.0, 0.0) },
+            },
         }
     }
 }
@@ -28,9 +138,9 @@ impl TemplateApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
 
         Default::default()
     }
@@ -39,70 +149,29 @@ impl TemplateApp {
 impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+        // eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+
+    /*
+    We need
+    1. A box widget
+    1. An arrow widget
+    1. All widgets need to be ided
+    1. In each frame, we need to:
+        a. know current position of every widget
+        b. know if anything is being dragged <not sure how to implement>
+        c.
+    */
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+        // println!("pre draf {:?}", self.pan_offset);
+        egui::CentralPanel::default()
+            .show(ctx, |ui| {
+                egui::Frame::canvas(ui.style()).show(ui, |ui| {
+                    self.root.draw(ui);
+                })
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
 
         if false {
             egui::Window::new("Window").show(ctx, |ui| {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::time::Duration;
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
@@ -11,7 +13,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(code_c4::TemplateApp::new(cc))),
     )
 }
 
@@ -30,9 +32,62 @@ fn main() {
         eframe::start_web(
             "the_canvas_id", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+            Box::new(|cc| Box::new(code_c4::TemplateApp::new(cc))),
         )
-        .await
-        .expect("failed to start eframe");
+            .await
+            .expect("failed to start eframe");
     });
+}
+
+peg::parser! {
+  pub grammar c4lang() for str {
+    pub rule identifier() -> String
+      = n:$([^'0'..='9']['0'..='9'|'a'..='z' | 'A'..='Z']+) {? Ok(n.to_string()) };
+
+    rule quoted_string() -> &'input str = "\"" n:$([^'"']+) "\"" { n }
+    rule non_ws() -> &'input str = n:$([^' ']+)
+
+    pub rule string() -> String =
+        "\"" n:$([^'"']+) "\"" { n.to_string() }
+        / n:$([^' ']+) { n.to_string() }
+
+    pub rule whitespaces() -> &'input str =
+        n: $(" "*) { n }
+
+    pub rule name_assignment() -> (String, String)
+        = i: identifier() ".name" whitespaces() "=" whitespaces() v: string() {
+            (i, v)
+        }
+    pub rule point_to() -> Option<String> =
+        ['-']+ i: identifier() ['-']+ ">" { Option::Some(i) }
+        / ['-']+ ">" { Option::None };
+
+    pub rule relationship() -> CArrow = a: identifier() whitespaces() p: point_to() whitespaces() b: identifier() {
+            CArrow {
+                name: p,
+                from: CBox { name: a },
+                to: CBox { name: b },
+            }
+        }
+  }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CBox {
+    name: String,
+}
+#[derive(Debug, PartialEq)]
+pub struct CArrow {
+    name: Option<String>,
+    from: CBox,
+    to: CBox,
+}
+
+#[test]
+fn test_parser() {
+    assert!(c4lang::identifier("11aa2").is_err());
+    assert_eq!(c4lang::string(r#""mamamia is a 1002.jjd££4""#), Ok("mamamia is a 1002.jjd££4".to_string()));
+    assert_eq!(c4lang::string(r#"mamamia££4"#), Ok("mamamia££4".to_string()));
+    let o = CArrow { name: None, from: CBox { name: "cow".to_string() }, to: CBox { name: "fresh".to_string() } };
+    assert_eq!(c4lang::relationship(r#"cow -------> fresh"#), Ok(o));
 }
